8d6ab5269167cea25a11f7e3d695cbc68b01c6ea
Akash <akashmane12598@gmail.com>
2020-10-15 02:04:16 +0530

    [Akash] Refactor. Change Mood Message Dynamically using Reflection UC7

--- MoodAnalyzer/MoodAnalyserFactory.cs
+++ MoodAnalyzer/MoodAnalyserFactory.cs
@@ -8,14 +8,41 @@ namespace MoodAnalyzer
 {
     public class MoodAnalyserFactory
     {
-        public static object CreateMoodAnalyser(string className, string constructor, string message)
+        //UC4
+        public static object CreateMoodAnalyser(string className, string constructor)
+        {
+            string pattern = @"." + constructor + "$";
+            if (Regex.IsMatch(className, pattern))
+            {
+                try
+                {
+                    Assembly assembly = Assembly.GetExecutingAssembly();
+                    //Type moodAnalyseType = assembly.GetType(className);
+                    Type moodAnalyseType = assembly.GetType(className);
+                    return Activator.CreateInstance(moodAnalyseType);
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class Name not found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor Name not found");
+            }
+        }
+
+
+
+        //UC5
+        public static object CreateMoodAnalyser_ParameterizedConstructor(string className, string constructor, string message)
         {
             Type type = Type.GetType("MoodAnalyzer.MoodAnalyser");
-            if(type.FullName.Equals(className) || type.Name.Equals(className))
+            if (type.FullName.Equals(className) || type.Name.Equals(className))
             {
                 if (type.Name.Equals(constructor))
                 {
-                    ConstructorInfo constructorInfo = type.GetConstructor(new [] { typeof(string) });
+                    ConstructorInfo constructorInfo = type.GetConstructor(new[] { typeof(string) });
                     object instance = constructorInfo.Invoke(new object[] { message });
                     return instance;
                 }
@@ -31,22 +58,25 @@ namespace MoodAnalyzer
 
         }
 
+        //UC6
         public static string InvokeAnalyseMethod(string methodName, string message)
         {
             try
             {
                 Type type = Type.GetType("MoodAnalyzer.MoodAnalyser");
-                object moodAnalyser = MoodAnalyserFactory.CreateMoodAnalyser("MoodAnalyzer.MoodAnalyser", "MoodAnalyser", message);
+                object moodAnalyser = MoodAnalyserFactory.CreateMoodAnalyser_ParameterizedConstructor("MoodAnalyzer.MoodAnalyser", "MoodAnalyser", message);
                 MethodInfo methodInfo = type.GetMethod(methodName);
                 object method = methodInfo.Invoke(moodAnalyser, null);
                 return method.ToString();
             }
             catch (NullReferenceException)
             {
-                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD,"Method Not Found");
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Method Not Found");
             }
         }
 
+
+        //UC7
         public static string SetField(string fieldName, string message)
         {
             try
@@ -62,7 +92,7 @@ namespace MoodAnalyzer
                 field.SetValue(moodAnalyser,message);
                 return moodAnalyser.message;  //Here, message is the variable of MoodAnalyse class
             }
-            catch (Exception)
+            catch (NullReferenceException)
             {
                 throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_FIELD,"Mentioned Field isn't there");
             }
